4c5c33aff78ac0f98304cfb6d5455d73994adabd
kanduriabhilash <34536565+kanduriabhilash@users.noreply.github.com>
2022-11-04 20:47:26 -0400

    Add files via upload

---
+++ Abhilash_c0880745_LabTest/Abhilash_c0880745_LabTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Abhilash_c0880745_LabTest
+//
+//  Created by MacStudent on 2022-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+}
+
